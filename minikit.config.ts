00ad587a08abecff84b698f4e180892e532218da
araokaman <araokaman.com@gmail.com>
2025-12-17 15:58:47 +0900

    submit

--- minikit.config.ts
+++ minikit.config.ts
@@ -15,8 +15,8 @@ export const minikitConfig = {
   },
   miniapp: {
     version: "1",
-    name: "Cubey", 
-    subtitle: "Your AI Ad Companion", 
+    name: "Kagey!!", 
+    subtitle: "Kage AI Ad Companion", 
     description: "Ads",
     screenshotUrls: [`${ROOT_URL}/screenshot-portrait.png`],
     iconUrl: `${ROOT_URL}/blue-icon.png`,
